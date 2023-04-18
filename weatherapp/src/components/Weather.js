f3c701f0326d05e39a37042376b02a3e7fdc96de
ka1979 <akproductions2002@gmail.com>
2023-04-18 08:28:41 -0400

    .
    .

--- weatherapp/src/components/Weather.js
+++ weatherapp/src/components/Weather.js
@@ -1,76 +1,147 @@
-import { useState,useEffect } from "react"
-const Weather=()=>{
- const [locationInfo, changeLocation]=useState()
- const [weatherCordinates, ChnageCordinates]=useState()
-useEffect(()=>{
+import { useState, useEffect } from "react";
+import WeatherInfo from "./WeatherInfo";
+import Select from 'react-select';
 
-    const getData=async()=>{
-        try{
-    if(locationInfo!==undefined){
+const Weather = () => {
+  const [locationInfo, changeLocation] = useState();
+  const [weatherCordinates, ChnageCordinates] = useState();
+  const [selectedState, setSelectedState] = useState(null);
 
-   
-     if (  locationInfo.length===2){
-       
-        console.log(locationInfo+"sfdsfdsfs")
-        const cityName=locationInfo[1]
-        const State=locationInfo[0]
-          const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${State},US&limit=5&appid=${process.env.REACT_APP_WEATHER_LOCATION_API_KEY}`)
-          const jsonData = await response.json();
-         
-        ChnageCordinates({
-            lat:jsonData.lat,
-            long:jsonData.lon
-        })
-         
-     }else if(locationInfo.length==1){
-        const zipcode=String(locationInfo[0])
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&limit=5&appid=${process.env.REACT_APP_WEATHER_LOCATION_API_KEY}`)
-        const jsonData = await response.json();
-        ChnageCordinates({
-            lat:jsonData.lat,
-            long:jsonData.lon
-        })
-        console.log(weatherCordinates)
-     }
-    }
-    }catch(error){
-        console.error(error)
-    }
-      }
-      getData()
-      return () => {
-        // this now gets called when the component unmounts
-      };
-    }, [locationInfo]);
-   
-      const validateForm = (event) => {
-        event.preventDefault();
+  const handleStateChange = (selectedOption) => {
+    setSelectedState(selectedOption);
+  }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        if (locationInfo !== undefined) {
+          if (locationInfo.length === 2) {
+            const cityName = locationInfo[1];
+            const State = locationInfo[0];
+            const response = await fetch(
+              `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${State},US&limit=5&appid=${process.env.REACT_APP_WEATHER_LOCATION_API_KEY}`
+            );
+          
+            const jsonData = await response.json();
+
+            ChnageCordinates({
+              lat: jsonData["0"]["lat"],
+              long: jsonData["0"]["lon"],
+            });
         
-        const stateInput = document.forms["myForm"]["state"].value;
-        const cityInput = document.forms["myForm"]["city"].value;
-        const zipcode=document.forms["myForm"]["zipcode"].value;
-        if (stateInput === "" && cityInput === "" && zipcode!=="") {
-            changeLocation([zipcode]); // Allow submission if only zipcode is filled
-            return;
+          } else if (locationInfo.length == 1) {
+            const zipcode = String(locationInfo[0]);
+            const response = await fetch(
+              `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&limit=5&appid=${process.env.REACT_APP_WEATHER_LOCATION_API_KEY}`
+            );
+            const jsonData = await response.json();
+            
+      
+            ChnageCordinates({
+              lat: jsonData.lat,
+              long: jsonData.lon,
+            });
+          }
         }
-        else if (stateInput !== "" && cityInput !== "") {
-            changeLocation([stateInput,cityInput]);
-          return; // Allow submission if both state and city are filled
-        }
-        else {
-          alert("Please fill out either just the zipcode or both the state and city.");
-          return false; // Prevent submission if requirements are not met
-        }
+      } catch (error) {
+        console.error(error);
       }
+    };
+    getData();
+    return () => {
+      // this now gets called when the component unmounts
+    };
+  }, [locationInfo]);
 
-    return( <>
-    <form id="myForm" onSubmit={validateForm}>
-  <input type="number" placeholder="enter zipcode" name="zipcode"></input>
-  <input type="text" placeholder="enter State" name="state"></input>
-  <input type="text" placeholder="enter City" name="city"></input>
-  <button type="submit">Submit</button>
-</form>
-  </> )
-}
+  const validateForm = (event) => {
+    event.preventDefault();
+
+    let cityInput = document.forms["myForm"]["city"].value;
+    let zipcode = document.forms["myForm"]["zipcode"].value;
+    if (selectedState === null && cityInput === "" && zipcode !== "") {
+      changeLocation([zipcode]); // Allow submission if only zipcode is filled
+      return;
+    } else if (selectedState !== null && cityInput !== "") {
+      changeLocation([selectedState.value, cityInput]);
+      return; // Allow submission if both state and city are filled
+    } else {
+      alert(
+        "Please fill out either just the zipcode or both the state and city."
+      );
+      return false; // Prevent submission if requirements are not met
+    }
+  };
+
+  return (
+    <>
+      <form id="myForm" onSubmit={validateForm}>
+        <WeatherInfo place={locationInfo} location={weatherCordinates}></WeatherInfo>
+        <input type="number" placeholder="enter zipcode" name="zipcode"></input>
+        <input type="text" placeholder="enter City" name="city"></input>
+        <Select
+        id="state"
+        name="states"
+        options={stateList}
+        value={selectedState}
+        onChange={handleStateChange}
+      />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+const stateList = [
+  {value: "AL", label: "Alabama"},
+  {value: "AK", label: "Alaska"},
+  {value: "AZ", label: "Arizona"},
+  {value: "AR", label: "Arkansas"},
+  {value: "CA", label: "California"},
+  {value: "CO", label: "Colorado"},
+  {value: "CT", label: "Connecticut"},
+  {value: "DE", label: "Delaware"},
+  {value: "FL", label: "Florida"},
+  {value: "GA", label: "Georgia"},
+  {value: "HI", label: "Hawaii"},
+  {value: "ID", label: "Idaho"},
+  {value: "IL", label: "Illinois"},
+  {value: "IN", label: "Indiana"},
+  {value: "IA", label: "Iowa"},
+  {value: "KS", label: "Kansas"},
+  {value: "KY", label: "Kentucky"},
+  {value: "LA", label: "Louisiana"},
+  {value: "ME", label: "Maine"},
+  {value: "MD", label: "Maryland"},
+  {value: "MA", label: "Massachusetts"},
+  {value: "MI", label: "Michigan"},
+  {value: "MN", label: "Minnesota"},
+  {value: "MS", label: "Mississippi"},
+  {value: "MO", label: "Missouri"},
+  {value: "MT", label: "Montana"},
+  {value: "NE", label: "Nebraska"},
+  {value: "NV", label: "Nevada"},
+  {value: "NH", label: "New Hampshire"},
+  {value: "NJ", label: "New Jersey"},
+  {value: "NM", label: "New Mexico"},
+  {value: "NY", label: "New York"},
+  {value: "NC", label: "North Carolina"},
+  {value: "ND", label: "North Dakota"},
+  {value: "OH", label: "Ohio"},
+  {value: "OK", label: "Oklahoma"},
+  {value: "OR", label: "Oregon"},
+  {value: "PA", label: "Pennsylvania"},
+  {value: "RI", label: "Rhode Island"},
+  {value: "SC", label: "South Carolina"},
+  {value: "SD", label: "South Dakota"},
+  {value: "TN", label: "Tennessee"},
+  {value: "TX", label: "Texas"},
+  {value: "UT", label: "Utah"},
+  {value: "VT", label: "Vermont"},
+  {value: "VA", label: "Virginia"},
+  {value: "WA", label: "Washington"},
+  {value: "WV", label: "West Virginia"},
+  {value: "WI", label: "Wisconsin"},
+  {value: "WY", label: "Wyoming"}
+];
+
+
 
 export default Weather;
--- weatherapp/src/components/WeatherInfo.js
+++ weatherapp/src/components/WeatherInfo.js
@@ -1,9 +1,43 @@
-const WeatherInfo=()=>{
+import { useState, useEffect } from "react";
+import { v4 as uiud } from "uuid";
 
-    return(
-        <></>
-    )
-}
+const WeatherInfo = (props) => {
+  const [weatherData, changeWeatherData] = useState();
+//   const InterpretData=()=>{
+//     const items=[]
+//     if(weatherData && props.place){
+        
+//        if (props.location.length==1){
+//         items.append(<p>Zipcode: {props.place[0]} as of </p>)
+//        }else{
+//         items.append(<p> {props.place[0]}, {props.place[1]} as of </p>)
 
+//        }
+//     }
+//   }
+  useEffect(() => {
+    const getWeatherInfo = async () => {
+      try {
+        if (props.location !== undefined) {
+       
+            console.log( `https://api.openweathermap.org/data/2.5/onecall?lat=${props.location.lat}&lon=${props.location.long}&exclude=minutely,alerts&appid=${process.env.REACT_APP_WEATHER_LOCATION_API_KEY}&units=imperial`)
+          const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${props.location.lat}&lon=${props.location.long}&exclude=minutely,alerts&appid=${process.env.REACT_APP_WEATHER_LOCATION_API_KEY}&units=imperial`
+          );
+          const jsonData = await response.json();
+          changeWeatherData(jsonData);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getWeatherInfo();
+    return () => {
+      // this now gets called when the component unmounts
+    };
+  }, [props.location]);
+
+  return <>{console.log(weatherData)}</>;
+};
 
 export default WeatherInfo;
